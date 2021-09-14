3a6e3e43cd3fc0ee7185fc821dfc3aa884e6e7e6
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-09-14 14:13:37 +1000

    Add plugin widget for activating keybindings

--- napari_midi/_dock_widget.py
+++ napari_midi/_dock_widget.py
@@ -6,36 +6,37 @@ see: https://napari.org/docs/dev/plugins/hook_specifications.html
 
 Replace code below according to your needs.
 """
-from napari_plugin_engine import napari_hook_implementation
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
+import napari
+import numpy as np
 from magicgui import magic_factory
+from napari_plugin_engine import napari_hook_implementation
+
+from .xtouchmini import XTouchMini
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+def new_label(layer):
+    """Set the currently selected label to the largest used label plus one."""
+    layer.selected_label = np.max(layer.data) + 1
 
 
 @magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def activate_default_bindings(viewer: napari.Viewer, labels_layer: "napari.layers.Labels"):
+    xt = XTouchMini(viewer)
+    xt.bind_current_step('b', 0, 0, 1)
+    xt.bind_current_step('b', 1, 2, 3)
+    xt.bind_slider('b')
+    xt.bind_button('b', (0, 0), viewer.dims, 'ndisplay', attr_value=3)
+    xt.bind_button('b', (0, 1), viewer.dims, 'ndisplay', attr_value=2)
+    xt.bind_button('b', (1, 2), labels_layer, 'visible')
+    xt.bind_button('b', (1, 0), labels_layer, 'mode', attr_value='erase')
+    xt.bind_button('b', (2, 0), labels_layer, 'mode', attr_value='paint')
+    xt.bind_button('b', (1, 1), labels_layer, 'mode', attr_value='fill')
+    xt.bind_button('b', (2, 1), labels_layer, 'mode', attr_value='pick')
+    xt.bind_button('b', (2, 2), labels_layer, 'mode', attr_value='pan_zoom')
+    xt.bind_action('b', (2, 3), new_label, (labels_layer,))
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     # you can return either a single widget, or a sequence of widgets
-    return [ExampleQWidget, example_magic_widget]
+    return activate_default_bindings
